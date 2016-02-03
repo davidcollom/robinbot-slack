6d874591c4c80aa2e0d53929d56055f3432a466d
David Collom <david@collom.co.uk>
2016-02-03 14:20:43 +0000

    Add in support for sleeping

--- robinbot.rb
+++ robinbot.rb
@@ -7,19 +7,33 @@ SlackRubyBot.configure do |config|
 end
 
 class RobinBot < SlackRubyBot::Bot
+  
+  def rb_sleep(sleep_time)
+    @sleeptimer = Time.now.getutc + sleep_time
+    logger.info("Robinbot sleeping until: #{@sleeptimer}")
+  end
+  def sleeping?
+    return false if @sleeptimer.nil?
+    diff = (Time.now.getutc - @sleeptimer)
+    (diff > 0)
+  end
+  
   match(/^robinbot$/i) do |c, d, m|
+    return if sleeping?
     logger.info "getting: 'http://robinbot.co.uk'"
     doc = open('http://robinbot.co.uk',&:read)
     c.say(text: "```#{doc}```", channel: d.channel)
   end
 
   match(/^robinbot(:?) (?<key>[a-zA-Z0-9]+(\-[a-zA-Z0-9]+)+)/i) do |c, d, m|
+    return if sleeping?
     logger.info "getting: 'http://robinbot.co.uk/#{m[:key]}'"
     doc = open("http://robinbot.co.uk/#{m[:key]}",&:read)
     c.say(text: "```#{doc}```", channel: d.channel)
   end
 
   command 'list' do |c, d, m|
+    return if sleeping?
     logger.info "getting: 'http://robinbot.co.uk/list'"
     doc = open('http://robinbot.co.uk/list',&:read)
     list = doc.scan(/(?<key>.*)=>(?<quote>.*)/)
@@ -28,13 +42,20 @@ class RobinBot < SlackRubyBot::Bot
   end
 
   command 'count' do |c, d, m|
+    return if sleeping?
     logger.info "getting: 'http://robinbot.co.uk/list'"
     doc = open('http://robinbot.co.uk/list',&:read)
     list = doc.scan(/(?<key>.*)=>(?<quote>.*)/)
     c.say(text: "I have #{list.size} quotes in my DB", channel: d.channel)
   end
   
+  command 'sleep' do |c, d, m|
+    c.say(text: "Sleeping for 60 seconds..", channel: d.channel)
+    rb_sleep(60)
+  end
+  
   match(/^robinbot(:?) (?<term>.*)/i) do |c,d,m|
+    return if sleeping?
     out = ''
     logger.info "getting list: 'http://robinbot.co.uk/list'"
     doc = open('http://robinbot.co.uk/list',&:read)
